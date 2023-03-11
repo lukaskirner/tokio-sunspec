eb4b050dce9d47239740f451fae3ad4fc7da4725
Lukas Kirner <lkirner@inovex.de>
2023-03-11 14:44:14 +0100

    Add RTU connect function

--- src/lib.rs
+++ src/lib.rs
@@ -10,12 +10,10 @@ use model::Model;
 use point::{Point, PointType};
 use std::{collections::HashMap, net::SocketAddr};
 use tokio_modbus::{client::Context, prelude::*};
+use tokio_serial::SerialStream;
 use types::Address;
 
 pub struct Client {
-    /// Connection string/address
-    pub socket_addr: SocketAddr,
-
     /// Slave Id of device
     pub slave_id: u8,
 
@@ -42,7 +40,36 @@ pub async fn connect_tcp(
 
     println!("SunSpec client connected to `{}`.", socket_addr);
     let mut client = Client {
-        socket_addr,
+        slave_id,
+        start_address,
+        models: HashMap::new(),
+        modbus_client: context.unwrap(),
+    };
+
+    println!("SunSpec checking for compatability ...");
+    let supported_models = client.model_discovery().await?;
+    client.models = supported_models;
+
+    return Ok(client);
+}
+
+pub async fn connect_rtu(
+    device_path: &str,
+    baud_rate: u32,
+    slave_id: u8,
+    start_address: Address,
+) -> Result<Client, Error> {
+    println!("Connecting SunSpec client ...");
+    let builder = tokio_serial::new(device_path, baud_rate);
+    let serial = SerialStream::open(&builder).unwrap();
+
+    let context = rtu::connect_slave(serial, Slave(slave_id)).await;
+    if context.is_err() {
+        return Err(Error::Io(context.err().unwrap()));
+    }
+
+    println!("SunSpec client connected to `{}`.", device_path);
+    let mut client = Client {
         slave_id,
         start_address,
         models: HashMap::new(),
